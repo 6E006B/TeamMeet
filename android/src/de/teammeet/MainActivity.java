6ac2d38bd5ca389fb20d1aa05c1738e738aed944
Christopher Schwardt <nookieman@gmx.de>
2012-04-10 22:46:16 +0200

    some toying with asmack examples

--- android/src/de/teammeet/MainActivity.java
+++ android/src/de/teammeet/MainActivity.java
@@ -20,13 +20,28 @@
 
 package de.teammeet;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManager;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	private String	CLASS	= MainActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -48,6 +63,13 @@ public class MainActivity extends Activity {
 				startSettingsActivity();
 			}
 		});
+
+		b = (Button) findViewById(R.id.buttonXMPP);
+		b.setOnClickListener(new View.OnClickListener() {
+			public void onClick(final View arg0) {
+				sendXMPPMessage();
+			}
+		});
 	}
 
 	protected void startMapActivity() {
@@ -58,5 +80,75 @@ public class MainActivity extends Activity {
 	protected void startSettingsActivity() {
 		final Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
 		startActivity(intent);
+	}
+
+	protected void sendXMPPMessage() {
+		new Thread(new Runnable() {
+			public void run() {
+
+				// try {
+				// SSLContext sc = SSLContext.getInstance("TLS");
+				// TrustManagerFactory tmf =
+				// TrustManagerFactory.getInstance(KeyManagerFactory
+				// .getDefaultAlgorithm());
+				// try {
+				// sc.init(null, tmf.getTrustManagers(), new SecureRandom());
+				// } catch (KeyManagementException e) {
+				// Log.e(CLASS, "KeyManagementException:\n" + e.getMessage());
+				// e.printStackTrace();
+				// }
+				// } catch (NoSuchAlgorithmException e1) {
+				// Log.e(CLASS, "NoSuchAlgorithmException:\n" +
+				// e1.getMessage());
+				// e1.printStackTrace();
+				// }
+
+				ConnectionConfiguration config = new ConnectionConfiguration("jabber.no");
+				config.setSelfSignedCertificateEnabled(true);
+				config.setDebuggerEnabled(true);
+				config.setSecurityMode(SecurityMode.disabled);
+				XMPPConnection xmpp = new XMPPConnection(config);
+				try {
+					xmpp.connect();
+
+					xmpp.login("teammeettest", "teammeettestpass");
+
+					Roster roster = xmpp.getRoster();
+
+					for (RosterEntry r : roster.getEntries()) {
+						Log.d(CLASS, r.getUser());
+					}
+
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					MultiUserChat muc = new MultiUserChat(xmpp, "teammeettestroom@conference.jabber.org");
+					// muc.create("ichbins");
+					// muc.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
+					muc.join("ichbins");
+					muc.invite("XXX@jabber.org", "hier");
+					muc.sendMessage("blablabla");
+
+					ChatManager chatManager = xmpp.getChatManager();
+					Chat chat = chatManager.createChat("XXX@jabber.org", new MessageListener() {
+
+						@Override
+						public void processMessage(Chat arg0, Message arg1) {
+							// TODO Auto-generated method stub
+							Log.d(CLASS, arg1.getBody());
+						}
+					});
+					Message message = new Message();
+					chat.sendMessage("huhu fuubaa");
+				} catch (XMPPException e) {
+					Log.e(CLASS, "Failed to login or send:\n" + e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		}).start();
 	}
 }
