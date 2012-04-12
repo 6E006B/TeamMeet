cbf5530e7dea2cf577dc8e94b2dad003abd60083
Christopher Schwardt <nookieman@gmx.de>
2012-04-12 22:50:42 +0200

    rename mServiceImpl to mServiceInterface

--- android/src/de/teammeet/xmpp/GroupMessageListener.java
+++ android/src/de/teammeet/xmpp/GroupMessageListener.java
@@ -4,24 +4,24 @@ import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.packet.Packet;
 
 import android.util.Log;
-
 import de.teammeet.service.ServiceInterfaceImpl;
 
 public class GroupMessageListener implements PacketListener {
 
-	private static String CLASS = GroupMessageListener.class.getSimpleName();
-	
-	ServiceInterfaceImpl mServiceImpl = null;
-	
+	private static String	CLASS			= GroupMessageListener.class.getSimpleName();
+
+	ServiceInterfaceImpl	mServiceInterface	= null;
+
 	public GroupMessageListener(ServiceInterfaceImpl serviceImpl) {
-		mServiceImpl = serviceImpl;
+		mServiceInterface = serviceImpl;
 	}
-	
+
 	@Override
 	public void processPacket(Packet packet) {
 		String from = packet.getFrom();
 		String xml = packet.toXML();
 		Log.d(CLASS, from + " sent " + xml);
+		// mServiceInterface.updateMate(from, location);
 	}
 
 }
