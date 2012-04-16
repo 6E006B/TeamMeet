1c6257e0656d3f52c7ac2014db91b6e56c8094e6
Christopher Schwardt <nookieman@gmx.de>
2012-04-16 21:05:50 +0200

    null XMPPService onServiceDisconnect

--- android/src/de/teammeet/MainActivity.java
+++ android/src/de/teammeet/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends Activity {
 																Log.d(CLASS,
 																		"MainActivity.ServiceConnection.onServiceDisconnected('" +
 																				className + "')");
+																mXMPPService = null;
 															}
 														};
 
