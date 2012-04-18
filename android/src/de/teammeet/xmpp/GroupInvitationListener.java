2b2a22d5887196b7335727ceeb6a33a4db0aa676
Christopher Schwardt <nookieman@gmx.de>
2012-04-18 13:01:35 +0200

    more meaningful parameter names for callback

--- android/src/de/teammeet/xmpp/GroupInvitationListener.java
+++ android/src/de/teammeet/xmpp/GroupInvitationListener.java
@@ -7,8 +7,8 @@ import org.jivesoftware.smackx.muc.InvitationListener;
 public class GroupInvitationListener implements InvitationListener {
 
 	@Override
-	public void invitationReceived(Connection arg0, String arg1, String arg2,
-			String arg3, String arg4, Message arg5) {
+	public void invitationReceived(Connection xmppConnection, String room, String inviter,
+			String reason, String password, Message message) {
 		// TODO Auto-generated method stub
 
 	}
