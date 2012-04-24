f53599f96c0c4e512b2e5ca1fb728080a2ebcb8e
Christopher Schwardt <nookieman@gmx.de>
2012-04-24 16:09:04 +0200

    add position following functionality
    MyDirectionLocationOverlay now has functionality to follow the position
    of the user on the map.

--- android/src/de/teammeet/MyDirectionLocationOverlay.java
+++ android/src/de/teammeet/MyDirectionLocationOverlay.java
@@ -6,20 +6,25 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.location.Location;
 
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 
 public class MyDirectionLocationOverlay extends MyLocationOverlay {
 
 	private MapView mMapView = null;
+	private MapController mMapController = null;
 	private Paint mPaintPlayer = null;
 	private double mArrowLength = 0;
+	private boolean mFollowLocation = false;
 	
 	public MyDirectionLocationOverlay(Context context, MapView mapView) {
 		super(context, mapView);
 		mMapView = mapView;
+		mMapController  = mMapView.getController();
 		Resources resources = mapView.getResources();
 		mPaintPlayer = new Paint();
 		mPaintPlayer.setStyle(Paint.Style.FILL);
@@ -49,6 +54,25 @@ public class MyDirectionLocationOverlay extends MyLocationOverlay {
 							Math.cos((-bearing - 90) * Math.PI / 180)));
 			path.close();
 			canvas.drawPath(path, mPaintPlayer);
+		}
+	}
+
+	public void followLocation(boolean active) {
+		mFollowLocation  = active;
+	}
+
+	@Override
+	public synchronized void onLocationChanged(Location location) {
+		super.onLocationChanged(location);
+		if (mFollowLocation) {
+			focusCurrentLocation();
+		}
+	}
+
+	public void focusCurrentLocation() {
+		final GeoPoint location = getMyLocation();
+		if (location != null) {
+			mMapController.animateTo(location);
 		}
 	}
 }
