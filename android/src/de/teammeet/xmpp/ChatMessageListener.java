1aabffc2f050f9f6c915e4669d40a9ec33a9552a
Christopher Schwardt <nookieman@gmx.de>
2012-04-27 03:32:00 +0200

    fix thread starting
    Threads are started by the start() function and not run().

--- android/src/de/teammeet/xmpp/ChatMessageListener.java
+++ android/src/de/teammeet/xmpp/ChatMessageListener.java
@@ -32,7 +32,7 @@ public class ChatMessageListener implements PacketListener {
 				public void run() {
 					mXMPPService.handleNewChatMessage(chatMessage);
 				}
-			}).run();
+			}).start();
 		}
 	}
 }
