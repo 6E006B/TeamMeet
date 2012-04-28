0c3294ef65f607bd4e62f7324e4bc9470c298b90
Christopher Schwardt <nookieman@gmx.de>
2012-04-28 23:48:05 +0200

    introduce MateOverlayItem
    This is a container for the ItemizedOverlay for each mate OverlayItem.

---
+++ android/src/de/teammeet/MateOverlayItem.java
@@ -0,0 +1,19 @@
+package de.teammeet;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.OverlayItem;
+
+public class MateOverlayItem extends OverlayItem {
+
+	private Mate mMate = null;
+
+	public MateOverlayItem(Mate mate) {
+		super(mate.getLocation(), mate.getID(), mate.getID());
+		mMate  = mate;
+	}
+
+	@Override
+	public GeoPoint getPoint() {
+		return mMate.getLocation();
+	}
+}
