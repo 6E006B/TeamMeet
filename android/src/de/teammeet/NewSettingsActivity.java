af41f66406871d6effbe8637df493cee8dd12423
Christopher Schwardt <nookieman@gmx.de>
2012-04-30 13:42:55 +0200

    use tabs as indentation

--- android/src/de/teammeet/NewSettingsActivity.java
+++ android/src/de/teammeet/NewSettingsActivity.java
@@ -5,16 +5,17 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
 public class NewSettingsActivity extends PreferenceActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        // Make sure default values are applied.  In a real app, you would
-        // want this in a shared function that is used to retrieve the
-        // SharedPreferences wherever they are needed.
-        PreferenceManager.setDefaultValues(this, R.xml.preference_settings, false);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.preference_settings);
-    }
+		// Make sure default values are applied.  In a real app, you would
+		// want this in a shared function that is used to retrieve the
+		// SharedPreferences wherever they are needed.
+		PreferenceManager.setDefaultValues(this, R.xml.preference_settings, false);
+
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.preference_settings);
+	}
 }
