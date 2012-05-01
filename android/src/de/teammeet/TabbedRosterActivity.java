3af3f93ca0594dce799139cbdca735fff2c2daa8
dtk <dtk@gmx.de>
2012-05-01 17:07:34 +0200

    Remove unused tab code

--- android/src/de/teammeet/TabbedRosterActivity.java
+++ android/src/de/teammeet/TabbedRosterActivity.java
@@ -1,6 +1,5 @@
 package de.teammeet;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 
@@ -8,7 +7,6 @@ import org.jivesoftware.smack.XMPPException;
 
 import android.app.AlertDialog;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
@@ -50,7 +48,6 @@ public class TabbedRosterActivity extends FragmentActivity implements TabHost.On
 	
 	private TabHost mTabHost;
 	private ViewPager mViewPager;
-	private HashMap<String, TabInfo> mapTabInfo = new HashMap<String, TabbedRosterActivity.TabInfo>();
 	private RosterAdapter mPagerAdapter;
 	
 	
@@ -123,46 +120,18 @@ public class TabbedRosterActivity extends FragmentActivity implements TabHost.On
 	
 
 	/**
-	 * Maintains extrinsic info of a tab's construct
-	 */
-	private class TabInfo {
-		private String tag;
-		private Class<?> clss;
-		private Bundle args;
-		private Fragment fragment;
-		TabInfo(String tag, Class<?> clazz, Bundle args) {
-			this.tag = tag;
-			this.clss = clazz;
-			this.args = args;
-		}
-
-	}
-	/**
 	 * A simple factory that returns dummy views to the Tabhost
-	 * @author mwho
 	 */
-	class TabFactory implements TabContentFactory {
-
-		private final Context mContext;
-
-		/**
-		 * @param context
-		 */
-		public TabFactory(Context context) {
-			mContext = context;
-		}
-
-		/** (non-Javadoc)
-		 * @see android.widget.TabHost.TabContentFactory#createTabContent(java.lang.String)
-		 */
+	private class DummyFactory implements TabContentFactory {
 		public View createTabContent(String tag) {
-			View v = new View(mContext);
-			v.setMinimumWidth(0);
-			v.setMinimumHeight(0);
-			return v;
+			View dummy = new View(TabbedRosterActivity.this);
+			dummy.setMinimumWidth(0);
+			dummy.setMinimumHeight(0);
+			return dummy;
 		}
 
 	}
+	
 	/** (non-Javadoc)
 	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
 	 */
@@ -176,13 +145,14 @@ public class TabbedRosterActivity extends FragmentActivity implements TabHost.On
 		setContentView(R.layout.tabbed_roster);
 		
 		// Initialise the TabHost
-		this.initialiseTabHost(savedInstanceState);
+		initialiseTabHost(savedInstanceState);
 		if (savedInstanceState != null) {
-			mTabHost.setCurrentTabByTag(savedInstanceState.getString(SAVED_TAB_KEY)); //set the tab as per the saved state
+			//set the tab as per the saved state
+			mTabHost.setCurrentTabByTag(savedInstanceState.getString(SAVED_TAB_KEY));
 		}
 		
 		// Intialise ViewPager
-		this.intialiseViewPager();
+		intialiseViewPager();
 		
 		mCurrentIntent = getIntent();
 	}
@@ -242,6 +212,32 @@ public class TabbedRosterActivity extends FragmentActivity implements TabHost.On
 	
 
 	/**
+	 * Initialise the Tab Host
+	 */
+	private void initialiseTabHost(Bundle args) {
+		mTabHost = (TabHost)findViewById(android.R.id.tabhost);
+		mTabHost.setup();
+		addTab(mTabHost.newTabSpec(CONTACTS_TAB_ID).setIndicator(getString(R.string.tab_contacts)));
+		addTab(mTabHost.newTabSpec(TEAMS_TAB_ID).setIndicator(getString(R.string.tab_teams)));
+		mTabHost.setOnTabChangedListener(this);
+	}
+
+	/**
+	 * Add Tab content to the Tabhost
+	 * @param activity
+	 * @param tabHost
+	 * @param tabSpec
+	 * @param clss
+	 * @param args
+	 */
+	private void addTab(TabHost.TabSpec tabSpec) {
+		// Attach a Tab view factory to the spec
+		tabSpec.setContent(new DummyFactory());
+		mTabHost.addTab(tabSpec);
+	}
+
+	
+	/**
 	 * Initialise ViewPager
 	 */
 	private void intialiseViewPager() {
@@ -255,37 +251,6 @@ public class TabbedRosterActivity extends FragmentActivity implements TabHost.On
 		this.mViewPager = (ViewPager)super.findViewById(R.id.viewpager);
 		this.mViewPager.setAdapter(this.mPagerAdapter);
 		this.mViewPager.setOnPageChangeListener(this);
-	}
-
-	/**
-	 * Initialise the Tab Host
-	 */
-	private void initialiseTabHost(Bundle args) {
-		mTabHost = (TabHost)findViewById(android.R.id.tabhost);
-		mTabHost.setup();
-		TabInfo tabInfo = null;
-		TabbedRosterActivity.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec(CONTACTS_TAB_ID).setIndicator(getString(R.string.tab_contacts)), ( tabInfo = new TabInfo(CONTACTS_TAB_ID, ContactsFragment.class, args)));
-		this.mapTabInfo.put(tabInfo.tag, tabInfo);
-		TabbedRosterActivity.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec(TEAMS_TAB_ID).setIndicator(getString(R.string.tab_teams)), ( tabInfo = new TabInfo(TEAMS_TAB_ID, Teams.class, args)));
-		this.mapTabInfo.put(tabInfo.tag, tabInfo);
-		// Default to first tab
-		//this.onTabChanged("Tab1");
-		//
-		mTabHost.setOnTabChangedListener(this);
-	}
-
-	/**
-	 * Add Tab content to the Tabhost
-	 * @param activity
-	 * @param tabHost
-	 * @param tabSpec
-	 * @param clss
-	 * @param args
-	 */
-	private static void AddTab(TabbedRosterActivity activity, TabHost tabHost, TabHost.TabSpec tabSpec, TabInfo tabInfo) {
-		// Attach a Tab view factory to the spec
-		tabSpec.setContent(activity.new TabFactory(activity));
-		tabHost.addTab(tabSpec);
 	}
 
 	public void onTabChanged(String tag) {
