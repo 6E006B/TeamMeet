f8bbff3cf716770490201ea5b0961ecf66ecf602
Christopher Schwardt <nookieman@gmx.de>
2012-05-06 20:25:19 +0200

    TeamMeetActivity now uses SherlockMapActivity
    This is a plugin for the ActionBarSherlock:
    https://github.com/JakeWharton/ActionBarSherlock-Plugin-Maps

--- android/src/de/teammeet/activities/teams/TeamMeetActivity.java
+++ android/src/de/teammeet/activities/teams/TeamMeetActivity.java
@@ -30,24 +30,23 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.google.android.maps.MapActivity;
+import com.actionbarsherlock.app.SherlockMapActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
 import de.teammeet.R;
-import de.teammeet.activities.preferences.SettingsActivity;
 import de.teammeet.helper.ToastDisposerSingleton;
 import de.teammeet.interfaces.IXMPPService;
 import de.teammeet.services.xmpp.XMPPService;
 
-public class TeamMeetActivity extends MapActivity {
+public class TeamMeetActivity extends SherlockMapActivity {
 
 	final String						CLASS						= TeamMeetActivity.class.getSimpleName();
 
@@ -91,6 +90,7 @@ public class TeamMeetActivity extends MapActivity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.Theme_Sherlock_Light_DarkActionBar);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mapview);
 
@@ -192,14 +192,14 @@ public class TeamMeetActivity extends MapActivity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(final Menu menu) {
-		MenuInflater inflater = getMenuInflater();
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getSupportMenuInflater();
 		inflater.inflate(R.menu.map, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(final MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item) {
 
 		switch (item.getItemId()) {
 			case R.id.goto_mylocation:
