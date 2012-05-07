7cf967517bf54c9c9ccee958dc1431231aa3948f
Christopher Schwardt <nookieman@gmx.de>
2012-05-08 00:14:16 +0200

    add TabsAdapter class to ChatsActivity
    This is handling the tabs of the chats.

--- android/src/de/teammeet/activities/chat/ChatsActivity.java
+++ android/src/de/teammeet/activities/chat/ChatsActivity.java
@@ -163,4 +163,80 @@ public class ChatsActivity extends SherlockFragmentActivity implements ViewPager
 		setTitle(chatInfo.getCounterpart());
 	}
 
+
+
+
+	public static class TabsAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener, ActionBar.TabListener {
+		private final Context mContext;
+		private final ActionBar mBar;
+		private final ViewPager mViewPager;
+		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+
+		static final class TabInfo {
+			private final Class<?> clss;
+			private final Bundle args;
+
+			TabInfo(Class<?> _class, Bundle _args) {
+				clss = _class;
+				args = _args;
+			}
+		}
+
+
+		public TabsAdapter(FragmentActivity activity, ActionBar bar, ViewPager pager) {
+			super(activity.getSupportFragmentManager());
+			mContext = activity;
+			mBar = bar;
+			mViewPager = pager;
+			mViewPager.setAdapter(this);
+			mViewPager.setOnPageChangeListener(this);
+		}
+
+		public void addTab(ActionBar.Tab tab, Class<? extends Fragment> clss, Bundle args) {
+			TabInfo info = new TabInfo(clss, args);
+			tab.setTag(info);
+			tab.setTabListener(this);
+			mTabs.add(info);
+			mBar.addTab(tab);
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public int getCount() {
+			return mTabs.size();
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			TabInfo info = mTabs.get(position);
+			return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+		}
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+		@Override
+		public void onPageSelected(int position) {
+			mBar.setSelectedNavigationItem(position);
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int state) {}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			Object tag = tab.getTag();
+			for (int i=0; i<mTabs.size(); i++) {
+				if (mTabs.get(i) == tag) {
+					mViewPager.setCurrentItem(i);
+				}
+			}
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {}
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {}
+	}
 }
