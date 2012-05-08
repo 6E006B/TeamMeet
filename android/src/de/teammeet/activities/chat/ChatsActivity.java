a07bc2f07d0eddb7fdfc43940c2d98769b9fa744
Christopher Schwardt <nookieman@gmx.de>
2012-05-08 12:02:22 +0200

    moved TabsAdapter to its own file

--- android/src/de/teammeet/activities/chat/ChatsActivity.java
+++ android/src/de/teammeet/activities/chat/ChatsActivity.java
@@ -2,18 +2,12 @@ package de.teammeet.activities.chat;
 
 import java.util.ArrayList;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 
@@ -179,82 +173,5 @@ public class ChatsActivity extends SherlockFragmentActivity implements ViewPager
 //		ChatInformation chatInfo = mChatInformationList.get(position);
 		mActionBar.setSelectedNavigationItem(position);
 //		setTitle(chatInfo.getCounterpart());
-	}
-
-
-
-
-	public static class TabsAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener, ActionBar.TabListener {
-		private final Context mContext;
-		private final ActionBar mBar;
-		private final ViewPager mViewPager;
-		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
-
-		static final class TabInfo {
-			private final Class<?> clss;
-			private final Bundle args;
-
-			TabInfo(Class<?> _class, Bundle _args) {
-				clss = _class;
-				args = _args;
-			}
-		}
-
-
-		public TabsAdapter(FragmentActivity activity, ActionBar bar, ViewPager pager) {
-			super(activity.getSupportFragmentManager());
-			mContext = activity;
-			mBar = bar;
-			mViewPager = pager;
-			mViewPager.setAdapter(this);
-			mViewPager.setOnPageChangeListener(this);
-		}
-
-		public void addTab(ActionBar.Tab tab, Class<? extends Fragment> clss, Bundle args) {
-			TabInfo info = new TabInfo(clss, args);
-			tab.setTag(info);
-			tab.setTabListener(this);
-			mTabs.add(info);
-			mBar.addTab(tab);
-			notifyDataSetChanged();
-		}
-
-		@Override
-		public int getCount() {
-			return mTabs.size();
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			TabInfo info = mTabs.get(position);
-			return Fragment.instantiate(mContext, info.clss.getName(), info.args);
-		}
-
-		@Override
-		public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-
-		@Override
-		public void onPageSelected(int position) {
-			mBar.setSelectedNavigationItem(position);
-		}
-
-		@Override
-		public void onPageScrollStateChanged(int state) {}
-
-		@Override
-		public void onTabSelected(Tab tab, FragmentTransaction ft) {
-			Object tag = tab.getTag();
-			for (int i=0; i<mTabs.size(); i++) {
-				if (mTabs.get(i) == tag) {
-					mViewPager.setCurrentItem(i);
-				}
-			}
-		}
-
-		@Override
-		public void onTabUnselected(Tab tab, FragmentTransaction ft) {}
-
-		@Override
-		public void onTabReselected(Tab tab, FragmentTransaction ft) {}
 	}
 }
