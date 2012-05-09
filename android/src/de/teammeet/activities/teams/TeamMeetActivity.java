fbf536c5e594ccae5c8b29d9dbe87a42ca082fd4
Christopher Schwardt <nookieman@gmx.de>
2012-05-09 12:45:48 +0200

    remove unneeded and (now) faulty import

--- android/src/de/teammeet/activities/teams/TeamMeetActivity.java
+++ android/src/de/teammeet/activities/teams/TeamMeetActivity.java
@@ -42,7 +42,6 @@ import com.actionbarsherlock.view.MenuItem;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
-import de.teammeet.MapGestureDetectorOverlay;
 import de.teammeet.R;
 import de.teammeet.helper.ActionBarHelper;
 import de.teammeet.interfaces.IXMPPService;
