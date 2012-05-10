8a124d03c37909c9f93c82b3a07c8f117412fd8f
Christopher Schwardt <nookieman@gmx.de>
2012-05-10 23:50:06 +0200

    improve IndicatorPacketExtension implementation

--- android/src/de/teammeet/services/xmpp/IndicatorPacketExtension.java
+++ android/src/de/teammeet/services/xmpp/IndicatorPacketExtension.java
@@ -4,27 +4,58 @@ import org.jivesoftware.smack.packet.PacketExtension;
 
 public class IndicatorPacketExtension implements PacketExtension {
 
+	public static final String NAMESPACE = "https://teammeet.de/teammeet_indicator.ns";
+	public static final String INDICATOR = "indicator";
+	public static final String LON = "lon";
+	public static final String LAT = "lat";
+	public static final String INFO = "info";
+
 	private int mLatitude = 0;
 	private int mLongitude = 0;
+	private String mInfo;
 
-	public IndicatorPacketExtension(int latitude, int longitude) {
+	public IndicatorPacketExtension(int latitude, int longitude, String info) {
 		mLatitude = latitude;
 		mLongitude  = longitude;
+		mInfo = info;
 	}
-	
+
 	@Override
 	public String getElementName() {
-		return "indicator";
+		return INDICATOR;
 	}
 
 	@Override
 	public String getNamespace() {
-		return "https://teammeet.de/teammeet.ns";
+		return NAMESPACE;
 	}
 
 	@Override
 	public String toXML() {
-		return String.format("<x xmlns=\"%s\"><indicator><lat>%d</lat><long>%d</long></indicator></x>", getNamespace(), mLatitude, mLongitude);
+		return String.format("<x xmlns=\"%s\">" +
+						     "<%s>" +
+				             "<%s>%d</%s>" +
+						     "<%s>%d</%s>" +
+						     "<%s>%s</%s>" +
+						     "</%s>" +
+				             "</x>",
+				             getNamespace(),
+				             INDICATOR,
+				             LON, mLongitude, LON,
+				             LAT, mLatitude, LAT,
+				             INFO, mInfo, INFO,
+				             INDICATOR);
 	}
 
+	public int getLatitude() {
+		return mLatitude;
+	}
+
+	public int getLongitude() {
+		return mLongitude;
+	}
+
+	public String getInfo() {
+		return mInfo;
+	}
 }
