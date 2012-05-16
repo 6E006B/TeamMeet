45920d3a1e56d706c80bc735d843c7fde6fbe2c8
Christopher Schwardt <nookieman@gmx.de>
2012-05-16 14:48:48 +0200

    dont draw anything in MapGestureDetectorOverlay
    The indicators get drawn in the IndicatorsOverlay.

--- android/src/de/teammeet/activities/teams/MapGestureDetectorOverlay.java
+++ android/src/de/teammeet/activities/teams/MapGestureDetectorOverlay.java
@@ -1,19 +1,12 @@
 package de.teammeet.activities.teams;
 
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.locks.ReentrantLock;
 
 import org.jivesoftware.smack.XMPPException;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.Typeface;
 import android.location.Address;
 import android.location.Geocoder;
 import android.util.Log;
@@ -24,9 +17,7 @@ import android.view.MotionEvent;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
-import com.google.android.maps.Projection;
 
-import de.teammeet.R;
 import de.teammeet.services.xmpp.XMPPService;
 
 public class MapGestureDetectorOverlay extends Overlay implements OnGestureListener {
@@ -35,40 +26,11 @@ public class MapGestureDetectorOverlay extends Overlay implements OnGestureListe
 	private GestureDetector gestureDetector;
 	private OnGestureListener onGestureListener;
 	private MapView mMapView = null;
-	private Context mContext = null;
 	private XMPPService mXMPPService = null;
-	private GeoPoint mIndicatorLocation = null;
 
-	private final ReentrantLock	mLock					= new ReentrantLock();
-
-	private List<String>		mAddressOfPoint			= null;
-	private Resources			mResources				= null;
-	private int					mIndicatorSize			= 0;
-	private int					mIndicatorWidth			= 0;
-	private Paint				mPaintIndicator			= null;
-	private Paint				mPaintIndicatorText		= null;
-	
 	public MapGestureDetectorOverlay(MapView mapView, Context context) {
-		gestureDetector = new GestureDetector(mContext, this);
+		gestureDetector = new GestureDetector(context, this);
 		mMapView = mapView;
-		mContext = context;
-		
-		mResources = mContext.getResources();
-		mIndicatorSize = mResources.getInteger(R.integer.indicator_size);
-		mIndicatorWidth = mResources.getInteger(R.integer.indicator_width);
-
-		mPaintIndicator = new Paint();
-		mPaintIndicator.setColor(mResources.getColor(R.color.paint_indicator));
-		mPaintIndicator.setStrokeWidth(mIndicatorWidth);
-		mPaintIndicator.setStyle(Paint.Style.STROKE);
-
-		mPaintIndicatorText = new Paint();
-		mPaintIndicatorText.setColor(mResources.getColor(R.color.paint_indicator_text));
-		mPaintIndicatorText.setAntiAlias(true);
-		mPaintIndicatorText.setTextSize(mResources.getInteger(R.integer.indicator_text_size));
-		mPaintIndicatorText.setTypeface(Typeface.DEFAULT_BOLD);
-		mPaintIndicatorText.setShadowLayer(mResources.getInteger(R.integer.indicator_text_border_width), 1,
-				1, mResources.getColor(R.color.paint_indicator_text_border));
 	}
 
 	public MapGestureDetectorOverlay(MapView mapView, Context context,
@@ -113,23 +75,22 @@ public class MapGestureDetectorOverlay extends Overlay implements OnGestureListe
 			float y = event.getY();
 			Log.d(CLASS, String.format("x: %f y: %f", x, y));
 			Log.d(CLASS, String.format("x: %d y: %d", (int)x, (int)y));
-			mIndicatorLocation  = mMapView.getProjection().fromPixels((int)x, (int)y);
+			GeoPoint indicatorLocation = mMapView.getProjection().fromPixels((int)x, (int)y);
 			Geocoder geoCoder = new Geocoder(mMapView.getContext(), Locale.getDefault());
 			try {
-				List<Address> addresses = geoCoder.getFromLocation(mIndicatorLocation.getLatitudeE6() / 1E6,
-						mIndicatorLocation.getLongitudeE6() / 1E6, 1);
+				List<Address> addresses =
+						geoCoder.getFromLocation(indicatorLocation.getLatitudeE6() / 1E6,
+						                         indicatorLocation.getLongitudeE6() / 1E6, 1);
 	
-				List<String> addressStringList = new LinkedList<String>();
 				String addressString = "";
 				if (addresses.size() > 0) {
 					for (int i = 0; i < addresses.get(0).getMaxAddressLineIndex(); i++) {
-						addressStringList.add(addresses.get(0).getAddressLine(i));
 						addressString += addresses.get(0).getAddressLine(i) + "\n";
 					}
+					addressString = addressString.substring(0, addressString.length()-1);
 				}
-				// Collections.reverse(addressStringList);
-				mAddressOfPoint = addressStringList;
-				mXMPPService.sendIndicator(mIndicatorLocation, addressString);
+				
+				mXMPPService.sendIndicator(indicatorLocation, addressString);
 			} catch (IOException e) {
 				Log.e(CLASS, "Error using Geocoder: " + e.getLocalizedMessage());
 				e.printStackTrace();
@@ -180,40 +141,5 @@ public class MapGestureDetectorOverlay extends Overlay implements OnGestureListe
 
 	public void setOnGestureListener(OnGestureListener onGestureListener) {
 		this.onGestureListener = onGestureListener;
-	}
-	
-	@Override
-	public void draw(Canvas canvas, MapView mapView, boolean shadow) {
-		super.draw(canvas, mapView, shadow);
-		if (mIndicatorLocation != null) {
-			acquireLock();
-			try {
-				Projection projection = mapView.getProjection();
-				final Point screenPoint = new Point();
-
-				projection.toPixels(mIndicatorLocation, screenPoint);
-				canvas.drawCircle(screenPoint.x, screenPoint.y, mIndicatorSize + 1, mPaintIndicator);
-
-				if (mAddressOfPoint != null) {
-					int x = screenPoint.x + mIndicatorSize + mIndicatorWidth;
-					int y = screenPoint.y - mIndicatorSize / 2 - mIndicatorWidth / 2;
-					for (String line : mAddressOfPoint) {
-						canvas.drawText(line, x, y, mPaintIndicatorText);
-						y -= mPaintIndicatorText.ascent() - mPaintIndicatorText.descent() * 2f;
-					}
-				}
-			} finally {
-				releaseLock();
-			}
-		}
-	}
-	
-
-	private void acquireLock() {
-		mLock.lock();
-	}
-
-	private void releaseLock() {
-		mLock.unlock();
 	}
 }
